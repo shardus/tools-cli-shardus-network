c4ce4e7b097a563c0562efdbdba9d5ca11f428e9
asyed94 <asyed94@gmail.com>
2019-06-21 14:25:42 -0500

    Makes network clean also clean .pm2 dir.

--- src/lib/network-clean.js
+++ src/lib/network-clean.js
@@ -8,10 +8,14 @@ module.exports = async () => {
     let instances = shell.ls('-d', `${instancesPath}/shardus-instance*`)
     let configPath = path.join(instancesPath, 'network-config.json')
     if (fs.existsSync(configPath)) {
+        // Delete '/db' and '/logs' dir for each instance
         for (let i = 0; i < instances.length; i++) {
             shell.rm('-rf', path.join(instances[i], 'db'))
             shell.rm('-rf', path.join(instances[i], 'logs'))
             console.log(`Cleaned state: ${path.basename(instances[i])}`)
         }
+        // Delete './pm2'
+        shell.rm('-rf', path.join(instancesPath, '.pm2'))
+        console.log(`Cleaned ./pm2`)
     } else console.log(`Error: Cannot find network-config file on current directory, please make sure to run the commands inside a network folder`)
 }
