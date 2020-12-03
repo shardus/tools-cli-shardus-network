542ebf2a0a8870e2ad6a8da746a30bfba9dd4353
Thant Sin Toe <mr.thantsintoe@gmail.com>
2020-12-03 16:53:27 +0630

    find network-cofig file in current working directory

--- src/actions/start.js
+++ src/actions/start.js
@@ -5,7 +5,7 @@ const create = require('../actions/create')
 const fs = require('fs')
 
 module.exports = function (args, options, logger) {
-  const networkDir = options.dir ? path.join(process.cwd(), options.dir) : path.join(process.cwd(), 'instances')
+  const networkDir = options.dir ? path.join(process.cwd(), options.dir) : process.cwd()
   if (fs.existsSync(networkDir)) {
     if (util.checkNetworkFolder(networkDir)) start(networkDir, parseInt(args.num), 'create', args.pm2)
     else create(args, Object.assign(options, {'noStart': false}), logger)
--- src/actions/stop.js
+++ src/actions/stop.js
@@ -4,7 +4,7 @@ const util = require('../lib/util')
 const fs = require('fs')
 
 module.exports = function (args, options, logger) {
-  const networkDir = options.dir ? path.join(process.cwd(), options.dir) : path.join(process.cwd(), 'instances')
+  const networkDir = options.dir ? path.join(process.cwd(), options.dir) : process.cwd()
   if (fs.existsSync(networkDir)) {
     if (util.checkNetworkFolder(networkDir)) stop(networkDir, args.num)
   } else {
